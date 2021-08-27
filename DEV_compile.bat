d6b47c6e074d7ba02fc43c8090aa5c66ddf59ccb
vincentIsGood <vincentdev324@gmail.com>
2021-08-27 12:15:32 +0800

    adding ooxml word renderer

--- DEV_compile.bat
+++ DEV_compile.bat
@@ -2,6 +2,6 @@
 
 set first=src/com/vincentcodes/markdown/*.java src/com/vincentcodes/markdown/renderer/*.java
 :: .java files are in encoding UTF-8
-javac -encoding UTF-8 -d classes -cp ./lib/*;./src/ %first%
+javac -encoding UTF-8 -d classes -cp ./lib/*;./src/ --release 11 %first%
 
 pause
---
+++ src/com/vincentcodes/markdown/renderer/OoxmlWordRenderer.java
@@ -0,0 +1,127 @@
+package com.vincentcodes.markdown.renderer;
+
+import com.vincentcodes.markdown.inline.TextNode;
+
+public class OoxmlWordRenderer implements Renderer{
+
+    @Override
+    public void body() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void done() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void hr() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void h1(TextNode texts) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void h2(TextNode texts) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void h3(TextNode texts) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void h4(TextNode texts) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void h5(TextNode texts) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void h6(TextNode texts) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void p(TextNode texts) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void append(String text) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void ol(TextNode[] texts) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void ul(TextNode[] texts) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void table(TextNode[] headings) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void tr(TextNode[] rowEntries) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void endTable() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void codeblock(String text, String lang) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void startBlockQuote() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void blockquote(TextNode text, int level) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void endBlockQuote() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
